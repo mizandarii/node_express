96604172cbb71a5b928d64e8325f23249d3ca47f
mizandarii <kristiina.parri@ivkhk.ee>
2025-06-16 13:58:59 +0300

    start using handlebars

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const path = require('path')
+const exphbs = require('express-handlebars')
+
 const app = express()
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'view', 'index.html'))
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'view', 'about.html'))
+    res.render('about')
 })
+
 
 const PORT = process.env.PORT || 3000
 
