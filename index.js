b30bf83689301617f38b125276b4b3018ead7101
mizandarii <kristiina.parri@ivkhk.ee>
2025-06-16 13:28:50 +0300

    2

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view', 'about.html'))
+})
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-    
+app.listen(PORT, () =>{
+    console.log(`Server is running on port $(PORT)`)
 })
